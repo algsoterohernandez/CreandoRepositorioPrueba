6519de3cc06bf815cfe952029ce4e2c5527ad453
DIR\a.carmona.garrido <ansuncg@gmail.com>
2023-03-31 12:50:19 +0200

    commit 2 Asun

--- PruebaRepositorio/src/main/java/alba/ClaseAsun.java
+++ PruebaRepositorio/src/main/java/alba/ClaseAsun.java
@@ -4,6 +4,7 @@ public class ClaseAsun {
 
     public static void main(String[] args) {
         System.out.println("Hola Muyayas");
+        System.out.println("Ya vamos a estar en la feria");
     }
 
 }
